8f022087073e989cfa32b5e1586ed6ca7cff13ae
hugo <hugo@localhost>
2000-08-17 15:10:31 +0000

    Replaced require by use and specified that HTML::Parser version 3 at least is required.

--- httpd/cgi-bin/checklink.pl
+++ httpd/cgi-bin/checklink.pl
@@ -5,7 +5,7 @@
 # (c) 1999-2000 World Wide Web Consortium
 # based on Renaud Bruyeron's checklink.pl
 #
-# $Id: checklink.pl,v 2.65 2000-08-15 18:03:27 hugo Exp $
+# $Id: checklink.pl,v 2.66 2000-08-17 15:10:31 hugo Exp $
 #
 # This program is licensed under the W3C(r) License:
 #	http://www.w3.org/Consortium/Legal/copyright-software
@@ -22,11 +22,11 @@
 use strict;
 
 package W3C::UserAgent;
-require LWP::UserAgent;
+use LWP::UserAgent;
 @W3C::UserAgent::ISA = qw(LWP::UserAgent);
 
 package W3C::CheckLink;
-require HTML::Parser;
+use HTML::Parser 3;
 @W3C::CheckLink::ISA = qw(HTML::Parser);
 
 # Autoflush
@@ -34,7 +34,7 @@ $| = 1;
 
 # Version info
 my $PROGRAM = 'W3C checklink';
-my $VERSION = q$Revision: 2.65 $ . '(c) 1999-2000 W3C';
+my $VERSION = q$Revision: 2.66 $ . '(c) 1999-2000 W3C';
 my $REVISION; ($REVISION = $VERSION) =~ s/Revision: (\d+\.\d+) .*/$1/;
 
 # Different options specified by the user
