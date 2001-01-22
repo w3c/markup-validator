2e8ce7d72753469c81b2cf6cfb56f68a675a2f25
hugo <hugo@localhost>
2001-01-22 15:58:28 +0000

    Made it clearer that www-validator is a mailing list to avoid confusion

--- httpd/cgi-bin/checklink.pl
+++ httpd/cgi-bin/checklink.pl
@@ -5,7 +5,7 @@
 # (c) 1999-2001 World Wide Web Consortium
 # based on Renaud Bruyeron's checklink.pl
 #
-# $Id: checklink.pl,v 2.71 2001-01-22 15:49:34 hugo Exp $
+# $Id: checklink.pl,v 2.72 2001-01-22 15:58:28 hugo Exp $
 #
 # This program is licensed under the W3C(r) License:
 #	http://www.w3.org/Consortium/Legal/copyright-software
@@ -21,7 +21,7 @@
 #
 # Comments and suggestions should be sent to the www-validator mailing list:
 #	mailto:www-validator@w3.org
-#	http://lists.w3.org/Archives/Public/www-validator/
+#	http://lists.w3.org/Archives/Public/www-validator/ (archives)
 
 use strict;
 
@@ -38,7 +38,7 @@ $| = 1;
 
 # Version info
 my $PROGRAM = 'W3C checklink';
-my $VERSION = q$Revision: 2.71 $ . '(c) 1999-2001 W3C';
+my $VERSION = q$Revision: 2.72 $ . '(c) 1999-2001 W3C';
 my $REVISION; ($REVISION = $VERSION) =~ s/Revision: (\d+\.\d+) .*/$1/;
 
 # Different options specified by the user
@@ -279,7 +279,9 @@ Options:
 	--help			Show this message.
 
 Documentation at: http://www.w3.org/2000/07/checklink
-Please send bug reports and comments to: $_contact_address
+Please send bug reports and comments to the www-validator mailing list:
+  $_contact_address
+  Archives are at: http://lists.w3.org/Archives/Public/www-validator/
 ";
     exit(0);
 }
@@ -1722,7 +1724,7 @@ sub html_footer() {
 <hr>
 <address>
 $PROGRAM $VERSION<br>
-Written by <a href=\"http://www.w3.org/People/Hugo/\">Hugo Haas</a>. Please send bug reports and comments to <a href=\"mailto:$_contact_address\">$_contact_address</a>.<br>
+Written by <a href=\"http://www.w3.org/People/Hugo/\">Hugo Haas</a>. Please send bug reports and comments to <a href=\"mailto:$_contact_address\">www-validator mailing list</a> (<a href=\"http://lists.w3.org/Archives/Public/www-validator/\">archives</a>).<br>
 Check out the <a href=\"http://www.w3.org/2000/07/checklink\">documentation</a>. Download the <a href=\"http://dev.w3.org/cvsweb/~checkout~/validator/httpd/cgi-bin/checklink.pl?rev=".$REVISION."&amp;content-type=text/plain\">source code</a> from the <a href=\"http://dev.w3.org/cvsweb/validator/httpd/cgi-bin/checklink.pl\">CVS log</a>.
 </address>
 </body>
