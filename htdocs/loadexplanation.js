017b559b9d1db8a18f9ff4493bb5eae06639433e
link <link@localhost>
2004-05-09 15:56:55 +0000

    Merging from branch validator-0_6_0-branch, at tag validator-0_6_5-release.

---
+++ htdocs/loadexplanation.js
@@ -0,0 +1,27 @@
+d=document
+w=window
+msgs=new Array()
+if (w && d && d.getElementsByTagName){
+ w.onload=doubleUp
+}
+function doubleUp() {
+ glist=d.getElementsByTagName("DIV")
+ upto(10)
+}
+function upto(n) {
+ if (n<glist.length) {
+  for (i=n;i<glist.length && i<n+10;i++) {
+   j=glist[i]
+   cls=j.className
+   if (cls) cls=cls.split(' ')
+   if (cls.length>1) {
+    if (cls[0]=='ve') msgs[cls[1]]=j.innerHTML
+    if (cls[0]=='hidden' && msgs[cls[1]]) {
+     j.className="ve "+cls[1]
+     j.innerHTML=msgs[cls[1]]
+    }
+   }
+  }
+  if (w && w.setTimeout) setTimeout('upto('+i+')',500)
+ }
+}
---
+++ misc/mkrelease.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+version="$1"
+if [ -z "$version" ] ; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+set -e
+
+tbversion=${version//./_}
+topdir=$(cd $(dirname $0)/.. ; pwd)
+tmpdir=
+trap cleanup EXIT
+
+cleanup()
+{
+    set +e
+    [ -z "$tmpdir" -o ! -d "$tmpdir" ] || rm -rf "$tmpdir"
+}
+
+tmpdir=$(mktemp -d /tmp/validator.XXXXXX)
+mkdir -p $tmpdir/validator-$version
+cp -pR $topdir/{htdocs,httpd,misc,README.cvs} $tmpdir/validator-$version
+
+cd $tmpdir
+
+find validator-$version -type d -name CVS | xargs -r rm -rf
+find validator-$version -name .cvsignore | xargs -r rm -rf
+rm -f validator-$version/misc/mkrelease.sh
+
+find . -type d | xargs -r chmod 755
+find . -type f | xargs -r chmod 644
+chmod 755 validator-$version/httpd/cgi-bin/check
+
+# sgml-lib tarball
+tar zc --owner=0 --group=0 -f $topdir/sgml-lib-$tbversion.tar.gz \
+  validator-$version/htdocs/sgml-lib
+
+# Validator tarball
+rm -rf validator-$version/htdocs/sgml-lib
+tar zc --owner=0 --group=0 -f $topdir/validator-$tbversion.tar.gz \
+  validator-$version
